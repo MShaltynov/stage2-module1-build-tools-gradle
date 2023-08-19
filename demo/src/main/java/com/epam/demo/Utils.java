ae78e7b36fec815e60f715f654b6455247b44956
Mikhail <shaltynovm@gmail.com>
2023-08-19 14:24:57 +0530

    errors are fixed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,12 +1,30 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
+    public static void main(String[] args) {
+        //List<String> strings = Arrays.asList("11.22", "2", "33", "87");
+        List<String> strings = new ArrayList<>();
+        strings.add(" ");
+        System.out.println(isAllPositiveNumbers(strings));
+
+    }
+
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-        for (String s:args) {
-            if (Integer.parseInt(s)<=0){
+
+        for (String s : args) {
+            if (s == null) {
+                return false;
+            }
+            try {
+                if (Double.parseDouble(s) <= 0) {
+                    return false;
+                }
+            } catch (Exception e) {
                 return false;
             }
         }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,23 @@
 package com.epam.utils;
 
 public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-         return Integer.parseInt(str)>0;
+    public static void main(String[] args) {
+       boolean a= isPositiveNumber(null);
+        System.out.println(a);
+    }
 
+    public static boolean isPositiveNumber(String str) {
+
+        if (str == null) {
+            return false;
+        }
+        try {
+            if (Double.parseDouble(str) <= 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
