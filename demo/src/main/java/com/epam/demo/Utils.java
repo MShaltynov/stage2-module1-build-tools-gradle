275a8dc5494f9f496fa49a960fe32656df52e1cf
Mikhail <shaltynovm@gmail.com>
2023-08-19 14:02:34 +0530

    initial commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,11 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        for (String s:args) {
+            if (Integer.parseInt(s)<=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
